739e38214e8784b7de4d8d2593101a1687590236
J S <smutek.jakub@seznam.cz>
2023-12-21 16:31:25 +0100

    a

--- src/FogOfWar.cpp
+++ src/FogOfWar.cpp
@@ -200,26 +200,6 @@ void FogOfWarV2::update( const std::vector<sf::Vector2f>& r_coords,
     reveal();
 }
 
-void insertionSort(std::vector<std::pair<float, float>>& arr, int n)
-{
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i].first;
-        j = i - 1;
- 
-        // Move elements of arr[0..i-1],
-        // that are greater than key, to one
-        // position ahead of their
-        // current position
-        while (j >= 0 && arr[j].first > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1].first = key;
-    }
-}
-
-
     void FogOfWarV2::update2( const std::vector<sf::Vector2f>& r_coords,
                              const std::vector<float>& radius,
                              const std::vector<int>& active_inds,
