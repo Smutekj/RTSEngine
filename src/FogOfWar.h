a3de1630e5fb49d7b0b46fe4fe8eb6fe4da3e92f
J S <smutek.jakub@seznam.cz>
2023-12-27 13:08:39 +0100

    Improved fog of war so that it can support multiple players. Also removed old implementation .

--- src/FogOfWar.h
+++ src/FogOfWar.h
@@ -51,111 +51,40 @@ template <int N> struct DeltaY {
 static constexpr auto DELTAS = DeltaY<N_MAX_DELTA_STRIPEIND>();
 } //! Namespace FOW
 
-class FogOfWar {
 
-    BloomEffect effect;
-
-  public:
-    float dy_;
-    float dx_;
-    int n_stripes;
-
-    struct StripeData {
-        float min_x;
-        float max_x;
-        float min_y;
-        float max_y;
-
-        StripeData() = default;
-        StripeData(float x_min, float x_max, float y_min, float y_max)
-            : min_x(x_min)
-            , max_x(x_max)
-            , min_y(y_min)
-            , max_y(y_max) {}
-    };
-
-    std::vector<std::vector<std::pair<float, float>>> stripes_;
-    std::vector<std::vector<StripeData>> stripe_data_;
-
-    std::vector<std::vector<int>> stripe_ind2grid_1dind_;
-    
-    sf::RectangleShape vision;
-    std::vector<bool> visited;
-
-    struct FOWGridData {
-        float x_min;
-        float x_max;
-    };
-
-    std::vector<std::array<FOWGridData, 500>> stripe_ind2fow_data_;
-    std::vector<int> stripe_ind2last_;
-    std::vector<int> active_stripe_inds_; 
-
-    sf::VertexArray fow_vertices_;  //! this holds vertices for drawing
-
-    FogOfWar(sf::Vector2i box_size, sf::Vector2f cell_size);
-
-    void update(const std::vector<sf::Vector2f>& vision_data, const std::vector<float>& radii, const std::vector<int>& active_inds);
-    void update2(const std::vector<sf::Vector2f>& vision_data, const std::vector<int>& active_inds);
-    void update3(const std::vector<sf::Vector2f>& vision_data, const std::vector<int>& active_inds);
-    void update4(const std::vector<sf::Vector2f>& vision_data, const std::vector<int>& active_inds);
-
-    void addToStripe2(int stripe_ind, float x_left, float x_right, float y_min, float y_max) ;
-    unsigned long long fucked = 0;
-    unsigned long long total = 0;
-
-    void addToStripe(int stripe_ind, float x_left, float x_right);
-    struct VisionData {
-        sf::Vector2f position;
-        float radius;
-    };
-
-    bool isVisible(const sf::Vector2f& r) const {
-        const auto stripe_ind = std::floor(r.y / dy_);
-        const auto& stripe = stripes_.at(stripe_ind);
-
-        int n_in_vision = 0;
-        for (const auto& [left, right] : stripe) {
-            n_in_vision += (left < r.x and r.x < right);
-        }
-        return n_in_vision == 1;
-    }
-
-    sf::Color grey_color = {0, 0, 1, 69};
-
-    void draw(sf::RenderWindow& window);
+struct VisionData {
+    sf::Vector2f r;
+    float radius_sq;
+    int player_ind = 0;
 };
 
 
+struct VisionField {
+
+    //! \struct left and right coordinates of the interval that the stripe 
+    struct Interval {
+        float x_left;
+        float x_right;
+    };
+
+    typedef std::deque<Interval> StripeVec;
+    std::array<StripeVec, FOW::N_STRIPES> stripes_;
+    std::array<StripeVec, FOW::N_STRIPES> revealed_stripes_;
+
+    sf::VertexArray fow_vertices_;  //! this holds vertices for drawing
+    sf::VertexArray revealed_fow_vertices_;  //! this holds vertices for drawing
+};
 
 class FogOfWarV2 {
 
   public:
     FogOfWarSettings settings_;
 
-    struct StripeData {
-        float y;
-        int next;
-    };
-
-    typedef std::deque<std::pair<float, float>> StripeVec;
-    std::array<StripeVec, FOW::N_STRIPES> stripes_;
-    std::array<StripeVec, FOW::N_STRIPES> revealed_stripes_;
-
-    sf::VertexArray fow_vertices_;  //! this holds vertices for drawing
-    sf::VertexArray revealed_fow_vertices_;  //! this holds vertices for drawing
-
-    FogOfWarV2(sf::Vector2i box_size, sf::Vector2f cell_size);
-
-    struct VisionData {
-        sf::Vector2f r;
-        float radius_sq;
-        int player_ind = 0;
-    };
+    std::vector<VisionField> player2vision_field_;
 
     std::array<std::vector<VisionData>, FOW::N_STRIPES> stripe2particle_data_;
-    std::array<int, FOW::N_STRIPES> stripe2;
 
+    FogOfWarV2(sf::Vector2i box_size, sf::Vector2f cell_size);
 
     void update(const std::vector<sf::Vector2f>& r_coords,
                 const std::vector<float>& radius,
@@ -165,13 +94,15 @@ class FogOfWarV2 {
                 const std::vector<float>& radius,
                 const std::vector<int>& active_inds,
                 const std::vector<int>& player_inds);
-    void addTo(StripeVec& stripe_vec, float x_left, float x_right);
-    
-    void reveal() ;
 
-    bool isVisible(const sf::Vector2f& r) const {
+    void addToStripe(int player_ind, int stripe_ind, float x_left, float x_right);
+    void addToRevealed(int player_ind, int stripe_ind, float x_left, float x_right);
+
+
+    bool isVisibleBy(int player_ind, const sf::Vector2f& r) const {
         const int stripe_ind = std::floor(r.y / dy_);
-        const auto& stripe = stripes_.at(stripe_ind);
+        const auto& stripes =  player2vision_field_.at(player_ind).stripes_;
+        const auto& stripe = stripes.at(stripe_ind);
 
         int n_in_vision = 0;
         for (const auto& [left, right] : stripe) {
@@ -180,11 +111,13 @@ class FogOfWarV2 {
         return n_in_vision == 1;
     }
     
-    void draw(sf::RenderWindow& window);
+    void draw(int player_ind, sf::RenderWindow& window);
 private:
-    void addToRevealed(int stripe_ind, float x_left, float x_right);
-    void addToStripe(int stripe_ind, float x_left, float x_right);
-    void drawStripe(FogOfWarV2::StripeVec& stripe, int stripe_ind, sf::VertexArray& vertices, sf::Color color); 
+    void addTo(VisionField::StripeVec& stripe_vec, float x_left, float x_right);
+    void reveal() ;
+
+
+    void drawStripe(VisionField::StripeVec& intervals, float y_pos, sf::VertexArray& vertices, sf::Color color, int& last);
 
     sf::Color grey_color = {0, 0, 1, 69};
     float dy_;
